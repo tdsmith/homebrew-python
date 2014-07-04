e57723a26c3c88288dbb366fa9b3575da7c0920e
Tim D. Smith <git@tim-smith.us>
2014-07-04 01:42:46 -0700

    pillow 2.5.0

--- pillow.rb
+++ pillow.rb
@@ -1,42 +1,49 @@
-require 'formula'
+require "formula"
 
 class Pillow < Formula
-  homepage 'https://github.com/python-imaging/Pillow'
-  url 'https://github.com/python-imaging/Pillow/archive/2.4.0.tar.gz'
-  sha1 '2e07dd7545177019331e8f3916335b69869e82b0'
-  head 'https://github.com/python-imaging/Pillow.git'
+  homepage "https://github.com/python-imaging/Pillow"
+  url "https://github.com/python-imaging/Pillow/archive/2.5.0.tar.gz"
+  sha1 "4ee5c6e22d2a19b31b39a967d639b2cb8c660921"
+  head "https://github.com/python-imaging/Pillow.git"
+
+  # waiting on upstream resolution of JPEG2000 issues
+  # https://github.com/python-pillow/Pillow/issues/767
+  # option "with-openjpeg", "Enable JPEG2000 support"
 
   depends_on :python => :recommended
   depends_on :python3 => :optional
-  depends_on 'freetype'
-  depends_on 'jpeg'
-  depends_on 'libtiff' => :recommended
-  depends_on 'little-cms2' => :recommended
-  depends_on 'openjpeg21' => :recommended
-  depends_on 'webp' => :recommended
+  depends_on "freetype"
+  depends_on "jpeg"
+  depends_on "libtiff" => :recommended
+  depends_on "little-cms2" => :recommended
+  depends_on "webp" => :recommended
+  # depends_on "homebrew/versions/openjpeg21" if build.with? "openjpeg"
 
   def install
-    # Help pillow find zlib and freetype2
     inreplace "setup.py" do |s|
       s.gsub! "ZLIB_ROOT = None", "ZLIB_ROOT = ('#{MacOS.sdk_path}/usr/lib', '#{MacOS.sdk_path}/usr/include')" unless MacOS::CLT.installed?
-      s.gsub! "LCMS_ROOT = None", "LCMS_ROOT = ('#{Formula["littlecms"].opt_prefix}/lib', '#{Formula["littlecms"].opt_prefix}/include')" if build.with? 'little-cms2'
+      s.gsub! "LCMS_ROOT = None", "LCMS_ROOT = ('#{Formula["littlecms"].opt_prefix}/lib', '#{Formula["littlecms"].opt_prefix}/include')" if build.with? "little-cms2"
       s.gsub! "JPEG_ROOT = None", "JPEG_ROOT = ('#{Formula["jpeg"].opt_prefix}/lib', '#{Formula["jpeg"].opt_prefix}/include')"
-      s.gsub! "JPEG2K_ROOT = None", "JPEG2K_ROOT = ('#{Formula["openjpeg21"].opt_prefix}/lib', '#{Formula["openjpeg21"].opt_prefix}/include')" if build.with? 'openjpeg21'
-      s.gsub! "TIFF_ROOT = None", "TIFF_ROOT = ('#{Formula["libtiff"].opt_prefix}/lib', '#{Formula["libtiff"].opt_prefix}/include')" if build.with? 'libtiff'
+      # s.gsub! "JPEG2K_ROOT = None", "JPEG2K_ROOT = ('#{Formula["openjpeg21"].opt_prefix}/lib', '#{Formula["openjpeg21"].opt_prefix}/include')" if build.with? "openjpeg"
+      s.gsub! "TIFF_ROOT = None", "TIFF_ROOT = ('#{Formula["libtiff"].opt_prefix}/lib', '#{Formula["libtiff"].opt_prefix}/include')" if build.with? "libtiff"
       s.gsub! "FREETYPE_ROOT = None", "FREETYPE_ROOT = ('#{Formula["freetype"].opt_prefix}/lib', '#{Formula["freetype"].opt_prefix}/include')"
     end
 
     ENV.append "CFLAGS", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers" unless MacOS::CLT.installed?
 
     Language::Python.each_python(build) do |python, version|
+      # don't accidentally discover openjpeg since it isn't working
+      system python, "setup.py", "build_ext", "--disable-jpeg2000" # if build.without? "openjpeg"
       system python, "setup.py", "install", "--prefix=#{prefix}", "--record=installed.txt", "--single-version-externally-managed"
     end
+
+    prefix.install "Tests"
   end
 
   test do
+    cp_r prefix/"Tests", testpath
     Language::Python.each_python(build) do |python, version|
-      # Only a small test until https://github.com/python-imaging/Pillow/issues/17
-      system python, "-c", "import PIL.Image"
+      system "#{python} -m nose Tests/test_*"
     end
   end
 end
