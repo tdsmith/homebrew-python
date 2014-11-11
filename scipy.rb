5fa678fd4afc24dc243d736bea05ff460ee2e2cd
Tim D. Smith <git@tim-smith.us>
2014-11-10 20:04:48 -0800

    scipy: fail brew test on test failures

--- scipy.rb
+++ scipy.rb
@@ -75,7 +75,7 @@ class Scipy < Formula
 
   test do
     Language::Python.each_python(build) do |python, version|
-      system python, "-c", "import scipy; scipy.test()"
+      system python, "-c", "import scipy; assert not scipy.test().failures"
     end
   end
 
