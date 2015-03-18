bbc763e7f9573b54ab99bec5d3044ab6c3691792
Tim D. Smith <git@tim-smith.us>
2015-03-17 23:53:41 -0700

    scapy 2.3.1
    Fixes #171. Fixes #210.

--- scapy.rb
+++ scapy.rb
@@ -1,15 +1,29 @@
-require 'formula'
-
 class Scapy < Formula
   homepage 'http://www.secdev.org/projects/scapy/'
-  url 'http://www.secdev.org/projects/scapy/files/scapy-2.2.0.tar.gz'
-  sha1 'ae0a9947a08a01a84abde9db12fed074ac888e47'
+  url "https://bitbucket.org/secdev/scapy/downloads/scapy-2.3.1.zip"
+  sha256 "8972c02e39a826a10c02c2bdd5025f7251dce9589c57befd9bb55c65f02e4934"
+  head "https://bitbucket.org/secdev/scapy", :using => :hg
 
-  depends_on :python
-  depends_on 'libdnet' => 'with-python'
-  depends_on 'pypcap'
+  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "libdnet"
+
+  resource "pylibpcap" do
+    url "https://downloads.sourceforge.net/project/pylibpcap/pylibpcap/0.6.4/pylibpcap-0.6.4.tar.gz"
+    sha256 "cfc365f2707a7986496acacf71789fef932a5ddbeaa36274cc8f9834831ca3b1"
+  end
 
   def install
-    system "python", 'setup.py', 'install', "--prefix=#{prefix}"
+    vendor_path = libexec/"vendor/lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", vendor_path
+    resource("pylibpcap").stage do
+      system "python", *Language::Python.setup_install_args(libexec/"vendor")
+    end
+    (lib/"python2.7/site-packages/homebrew-scapy-pylibpcap.pth").write "#{vendor_path}\n"
+    system "python", *Language::Python.setup_install_args(prefix)
+  end
+
+  test do
+    command = "rdpcap('#{test_fixtures("test.pcap")}')"
+    assert_match /TCP/, pipe_output(bin/"scapy", command)
   end
 end
