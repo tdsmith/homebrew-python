a5c69522b9a4001d4bd17eed7d30b2f9544b3b95
Tim D. Smith <git@tim-smith.us>
2015-04-08 09:30:28 -0700

    vpython 6.11
    Closes #217. Closes #199. Closes #155.
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- vpython.rb
+++ vpython.rb
@@ -1,13 +1,9 @@
-require "formula"
-
 class Vpython < Formula
-  url "https://downloads.sourceforge.net/project/vpythonwx/6.10-release/vpython-wx-src.6.10.tgz"
-  sha1 "d51ca07dad92a1ae9a3d5cc57af48b180f457228"
+  url "https://github.com/BruceSherwood/vpython-wx/archive/v6.11.tar.gz"
+  sha256 "5fcc87d401d6d39c9044979f2f9a5286a277504b26465eb6c89bfa7ec4a9783d"
   head "https://github.com/BruceSherwood/vpython-wx.git"
   homepage "http://vpython.org/"
 
-  # no simultaneous python2+3 support from Homebrew boost yet
-  depends_on :python
   depends_on "numpy"
   depends_on "wxpython"
 
@@ -16,18 +12,44 @@ class Vpython < Formula
   depends_on "boost" => boost_args
   depends_on "boost-python" => boost_args
 
+  resource "FontTools" do
+    url "https://pypi.python.org/packages/source/F/FontTools/FontTools-2.4.tar.gz"
+    sha256 "0a937fc607cbd31e30f45146e37adf4bdcd935565376bd8c626c9221d99fef8d"
+  end
+
+  resource "Polygon2" do
+    url "https://pypi.python.org/packages/source/P/Polygon2/Polygon2-2.0.7.zip"
+    sha256 "a779378f2258a8586f1a5967896c419c4c6859d8a067677259dacd3146453f24"
+  end
+
+  resource "TTFQuery" do
+    url "https://pypi.python.org/packages/source/T/TTFQuery/TTFQuery-1.0.5.tar.gz"
+    sha256 "d5b8d369903ee2754541819f27de8ea35486d124484e36a4869503d9a9ac7e4d"
+  end
+
   needs :cxx11
+
   # spurious errors because dependent numpy depends_on gcc through :fortran
   # https://github.com/Homebrew/homebrew/issues/30474
   cxxstdlib_check :skip
 
   def install
     ENV.cxx11
-    system "python", "setup.py", "install", "--prefix=#{prefix}", "--single-version-externally-managed",
-                     "--record=installed.txt"
+
+    vendor_packages = libexec/"vendor/lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", vendor_packages
+    resources.each do |r|
+      r.stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+    pth_contents = "#{vendor_packages}\n#{vendor_packages}/FontTools\n"
+    (lib/"python2.7/site-packages/homebrew-vpython-bundle.pth").write pth_contents
+
+    system "python", *Language::Python.setup_install_args(prefix)
   end
 
   test do
-    system "python", "-c", "import visual"
+    shell_output "python -c 'import visual'", 255
   end
 end
