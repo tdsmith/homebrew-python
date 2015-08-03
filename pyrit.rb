e5b32713f10814d4aeb2c7f92bc0982e3ccc684f
Michal Altmann <m.altmann@trillinis.com>
2015-08-03 08:25:06 -0700

    pyrit: modernize
    modernize pyrit
    
    Signed-off-by: Tim D. Smith <git@tim-smith.us>

--- pyrit.rb
+++ pyrit.rb
@@ -1,16 +1,17 @@
-require 'formula'
-
 class Pyrit < Formula
-  homepage 'https://code.google.com/p/pyrit/'
-  url 'https://pyrit.googlecode.com/files/pyrit-0.4.0.tar.gz'
-  sha1 '3041c0d593c21df88f41416be872eca0997cf22b'
+  desc "Pyrit allows to attack against WPA-PSK authentication"
+  homepage "https://code.google.com/p/pyrit/"
+  url "https://pyrit.googlecode.com/files/pyrit-0.4.0.tar.gz"
+  sha256 "eb1a21cb844b1ded3eab613a8e9d5e4ef901530b04668fb18fe82ed1b4afa7cc"
 
-  depends_on :python
-  depends_on 'libdnet' => 'with-python'
-  depends_on 'scapy'
+  depends_on "libdnet"
+  depends_on "scapy"
 
   def install
-      system "python", "setup.py", "build"
-      system "python", "setup.py", "install", "--prefix=#{prefix}"
+    system "python", *Language::Python.setup_install_args(prefix)
+  end
+
+  test do
+    system python, "-c", "import pyrit_cli"
   end
 end
