bf8354654eb982af64cc9cee945046a038903ec9
Baptiste Fontaine <batifon@yahoo.fr>
2015-11-07 14:16:17 +0100

    matplotlib-basemap: audit fixes
    Closes #260.
    
    Signed-off-by: Baptiste Fontaine <batifon@yahoo.fr>

--- matplotlib-basemap.rb
+++ matplotlib-basemap.rb
@@ -1,9 +1,8 @@
-require "formula"
-
 class MatplotlibBasemap < Formula
+  desc "Python library for plotting 2D data on maps"
   homepage "http://matplotlib.org/basemap"
   url "https://downloads.sourceforge.net/project/matplotlib/matplotlib-toolkits/basemap-1.0.7/basemap-1.0.7.tar.gz"
-  sha1 "e1d5750aab4b2d2c3191bba078a6ae3e2bafa068"
+  sha256 "e07ec2e0d63b24c9aed25a09fe8aff2598f82a85da8db74190bac81cbf104531"
   head "https://github.com/matplotlib/basemap.git"
 
   depends_on :python => :recommended
@@ -22,15 +21,15 @@ class MatplotlibBasemap < Formula
 
   resource "six" do
     url "https://pypi.python.org/packages/source/s/six/six-1.7.3.tar.gz"
-    sha1 "43d173ff19bf2ac41189aa3701c7240fcd1182e3"
+    sha256 "7a842c9f882c0b2ab1064d567bb9fff6a21c9efbc3d9992083ad6193787ed393"
   end
 
-  def package_installed? python, module_name
+  def package_installed?(python, module_name)
     quiet_system python, "-c", "import #{module_name}"
   end
 
   def install
-    if build.with? "python3" and not package_installed? "python3", "six"
+    if build.with?("python3") && !package_installed?("python3", "six")
       resource("six").stage do
         system "python3", "setup.py", "install", "--prefix=#{prefix}",
                           "--single-version-externally-managed",
@@ -42,13 +41,13 @@ class MatplotlibBasemap < Formula
       s.gsub! "GEOS_dir = None", "GEOS_dir = '#{Formula["geos"].opt_prefix}'"
     end
 
-    Language::Python.each_python(build) do |python, version|
+    Language::Python.each_python(build) do |python, _|
       system python, "setup.py", "install", "--prefix=#{prefix}", "--record=installed.txt"
     end
   end
 
   test do
-    Language::Python.each_python(build) do |python, version|
+    Language::Python.each_python(build) do |python, _|
       system python, "-c", "import mpl_toolkits.basemap.test as test; test.test()"
     end
   end
