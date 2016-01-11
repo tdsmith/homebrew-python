75404791501ec31a64956451d5864702bfc6e6bf
Baptiste Fontaine <batifon@yahoo.fr>
2016-01-11 12:20:23 +0100

    pygame: audit fixes

--- pygame.rb
+++ pygame.rb
@@ -1,38 +1,37 @@
 class Pygame < Formula
-  homepage 'http://pygame.org'
+  desc "Set of Python modules designed for writing video games"
+  homepage "http://pygame.org"
   url "https://bitbucket.org/pygame/pygame",
       :revision => "faa5879a7e6bfe10e4e5c79d04a3d2fb65d74a62",
       :using => :hg
   version "1.9.2a0"
-  head 'https://bitbucket.org/pygame/pygame', :using => :hg
+  head "https://bitbucket.org/pygame/pygame", :using => :hg
 
   option "without-python", "Build without python2 support"
   depends_on :python3 => :optional
-  depends_on 'sdl'
-  depends_on 'sdl_image'
-  depends_on 'sdl_mixer'
-  depends_on 'sdl_ttf'
-  depends_on 'smpeg'
-  depends_on 'jpeg'
-  depends_on 'libpng'
-  depends_on 'portmidi'
-  depends_on 'homebrew/python/numpy'
+  depends_on "sdl"
+  depends_on "sdl_image"
+  depends_on "sdl_mixer"
+  depends_on "sdl_ttf"
+  depends_on "smpeg"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "portmidi"
+  depends_on "homebrew/python/numpy"
   depends_on "freetype"
 
   def install
     # We provide a "Setup" file based on the "Setup.in" because the detection
     # code in config.py does not know about the HOMEBREW_PREFIX, assumes SDL
     # is built as a framework and cannot find the Frameworks inside of Xcode.
-    mv 'Setup.in', 'Setup'
+    mv "Setup.in", "Setup"
     sdl = Formula["sdl"].opt_prefix
     sdl_ttf = Formula["sdl_ttf"].opt_prefix
     sdl_image = Formula["sdl_image"].opt_prefix
     sdl_mixer = Formula["sdl_mixer"].opt_prefix
     smpeg = Formula["smpeg"].opt_prefix
-    png = Formula["libpng"].opt_prefix
-    jpeg = Formula["jpeg"].opt_prefix
     portmidi = Formula["portmidi"].opt_prefix
-    inreplace 'Setup' do |s|
+    inreplace "Setup" do |s|
       s.gsub!(/^SDL =.*$/, "SDL = -I#{sdl}/include/SDL -Ddarwin -lSDL")
       s.gsub!(/^FONT =.*$/, "FONT = -I#{sdl_ttf}/include/SDL -lSDL_ttf")
       s.gsub!(/^IMAGE =.*$/, "IMAGE = -I#{sdl_image}/include/SDL -lSDL_image")
@@ -49,8 +48,8 @@ class Pygame < Formula
     system "cat Setup_Darwin.in >> Setup"
 
     Language::Python.each_python(build) do |python, version|
-        ENV.prepend_create_path "PYTHONPATH", lib+"python#{version}/site-packages"
-        system python, *Language::Python.setup_install_args(prefix)
+      ENV.prepend_create_path "PYTHONPATH", lib+"python#{version}/site-packages"
+      system python, *Language::Python.setup_install_args(prefix)
     end
   end
 end
