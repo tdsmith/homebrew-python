82ee7a91e4b731624207135a8fac072a714dbed8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-25 07:46:49 +0100

    pysvn: add 1.9.2 bottle.

--- pysvn.rb
+++ pysvn.rb
@@ -4,6 +4,13 @@ class Pysvn < Formula
   url "http://pysvn.barrys-emacs.org/source_kits/pysvn-1.9.2.tar.gz"
   sha256 "4ff7a1983794107e2056abb008f6d0acd8ca5ac9126b815cf33388d0d80bb89e"
 
+  bottle do
+    cellar :any
+    sha256 "99abe8206b8974d5f38a822a2e2cccf07c00650a8ef8aab8e1b554ddfa88ff55" => :el_capitan
+    sha256 "6e92a01ee0d9668e14a3bc52b1f0567c3af2b551ad02619e7daed2869ff0c051" => :yosemite
+    sha256 "fd8995e6ecf029abaf5c24efe462e859d447af01f55d0f67e4447d3c277222aa" => :mavericks
+  end
+
   option "without-python", "Build without python2 support"
   depends_on :python3 => :optional
   depends_on "subversion"
