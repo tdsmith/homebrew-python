005f32a0e4d26eb7bc2365b95b05699646fde6e6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-25 08:13:45 +0100

    scapy: add 2.3.2 bottle.

--- scapy.rb
+++ scapy.rb
@@ -5,6 +5,13 @@ class Scapy < Formula
   sha256 "3de539ca67dd39e41287f0b36afd85188bd437604092726684c98debd816df01"
   head "https://github.com/secdev/scapy.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2ca5dbe42f8cee37f90918ff1405fba4ac5de0383c9e97d3aaa645d693985212" => :el_capitan
+    sha256 "289e7128c18924e591ed843c8bcb91cb2c6f15c45b0ec1858f84f15fc36ad27d" => :yosemite
+    sha256 "c7d010874cb8aea533d927b1a7874d4f2bf015dad112e8fa6163a2941d2cf7d9" => :mavericks
+  end
+
   depends_on :python
   depends_on "libdnet"
 
